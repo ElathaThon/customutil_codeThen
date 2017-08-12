0d9b7357d2779589eadc6e2979162b13b19ffa94
Elatha <bernat.90@gmail.com>
2017-08-12 18:44:11 +0200

    Added method processTemplate to MustacheUtil class

--- src/main/java/com/topoinventari/customutils/MustacheUtil.java
+++ src/main/java/com/topoinventari/customutils/MustacheUtil.java
@@ -1,10 +1,27 @@
 package com.topoinventari.customutils;
 
+import com.github.mustachejava.DefaultMustacheFactory;
+import com.github.mustachejava.Mustache;
+import com.github.mustachejava.MustacheFactory;
+
+import java.io.IOException;
+import java.io.StringWriter;
+
 /**
  * Created by elatha on 12/8/17.
  */
 public class MustacheUtil {
 
+    /** Takes template and subtitute the placehoders {{}} for the values */
+    public static String processTemplate(String filename, Object value) throws IOException {
 
+        MustacheFactory mustache = new DefaultMustacheFactory();
+        Mustache template = mustache.compile(filename);
+        StringWriter writer = new StringWriter();
+        template.execute(writer, value).flush();
+
+        return writer.toString();
+
+    }
 
 }
